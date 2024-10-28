61324e6d33745fe9223e7437d8cdefc1af2fa790
unknown <binkbop101@gmail.com>
2024-10-28 14:02:40 +0000

    EDIT: override the toString() method

--- src/main/java/com/lbg/Main.java
+++ src/main/java/com/lbg/Main.java
@@ -4,26 +4,29 @@ import com.lbg.classes.CarColour;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-        Car bartCar = new Car();
-        System.out.println(bartCar.getSpeed());
-        System.out.println(bartCar);
-        bartCar.setColour(CarColour.BRITISH_RACING_GREEN);
-        System.out.println(bartCar.getColour());
-        bartCar.accelerate(50);
-        System.out.println(bartCar.getSpeed());
-        bartCar.deAccelerate(34);
-        System.out.println(bartCar.getSpeed());
+//        System.out.println("Hello and welcome!");
+//        Car bartCar = new Car();
+//        System.out.println(bartCar.getSpeed());
+//        System.out.println(bartCar);
+//        bartCar.setColour(CarColour.BRITISH_RACING_GREEN);
+//        System.out.println(bartCar.getColour());
+//        bartCar.accelerate(50);
+//        System.out.println(bartCar.getSpeed());
+//        bartCar.deAccelerate(34);
+//        System.out.println(bartCar.getSpeed());
+//
+//        Car sally = new Car("997 Carrera S");
+//        sally.setColour(CarColour.BABY_BLUE);
+//        System.out.println(sally.getSpeed());
+//        System.out.println(sally.getModel());
+//
+//        Car[] cars = {bartCar, sally};
+//        for (Car car : cars){
+//            System.out.println(car.getColour());
+//        }
 
-        Car sally = new Car("997 Carrera S");
-        sally.setColour(CarColour.BABY_BLUE);
-        System.out.println(sally.getSpeed());
-        System.out.println(sally.getModel());
-
-        Car[] cars = {bartCar, sally};
-        for (Car car : cars){
-            System.out.println(car.getColour());
-        }
+        Car myCar = new Car("Honda", "Jazz", CarColour.RED);
+        System.out.println(myCar);
 
     }
 }
--- src/main/java/com/lbg/classes/Car.java
+++ src/main/java/com/lbg/classes/Car.java
@@ -2,13 +2,23 @@ package com.lbg.classes;
 
 public class Car {
 
-    public Car(String model){
+    public Car(String make, String model, CarColour colour)
+    {
+        this.setMake(make);
         this.setModel(model);
+        this.setColour(colour);
     }
+
     public Car(){
         this.make = "Unknown";
         this.setModel("Unknown");
     }
+    @Override
+    public String toString(){
+        String info = "Car: " + this.getMake();
+        return "I am a car";
+    }
+
     private int speed;
     private String make;
     private CarColour colour;
@@ -29,12 +39,16 @@ public class Car {
     }
 
     //setters
-    public void  setColour(CarColour colour){
-        this.colour = colour;
+    public void setMake(String make) {
+        this.make = make;
     }
     public void setModel(String model) {
         this.model = model;
     }
+    public void  setColour(CarColour colour){
+        this.colour = colour;
+    }
+
 
     //methods
     public void accelerate(int deltaV) {
